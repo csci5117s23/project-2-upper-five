a69152197b83ff647210b203ea12e2f1795f5334
Jason Woitalla <jason.woitalla@gmail.com>
2023-04-18 10:51:41 -0500

    A whole ton of changes with correct api usages

--- src/modules/fetcher.js
+++ src/modules/fetcher.js
@@ -1,6 +1,13 @@
-const fetcher = (url) => fetch(url).then((r) => r.json());
+const apiFetcher = (url) =>
+	fetch(url, {
+		method: "GET",
+		headers: {
+			"x-apikey": process.env.NEXT_PUBLIC_API_KEY,
+		},
+	}).then((r) => r.json());
 
 const tokenFetcher = ([url, token]) => {
+	console.log("Token Fetcher: " + url + " with: " + token);
 	return fetch(url, {
 		method: "GET",
 		headers: {
@@ -27,4 +34,54 @@ const imageFetcher = (url) => {
 		});
 };
 
-export { fetcher, tokenFetcher, imageFetcher };
+const postFetcher = ([url, token, body]) => {
+	return fetch(url, {
+		method: "POST",
+		headers: {
+			Authorization: `Bearer ${token}`,
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(body),
+	})
+		.then((r) => r.json())
+		.catch((e) => {
+			console.log("Error with POST fetcher: " + e);
+		});
+};
+
+const putFetcher = ([url, token, body]) => {
+	return fetch(url, {
+		method: "PUT",
+		headers: {
+			Authorization: `Bearer ${token}`,
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(body),
+	})
+		.then((r) => r.json())
+		.catch((e) => {
+			console.log("Error with PUT fetcher: " + e);
+		});
+};
+
+const deleteFetcher = ([url, token]) => {
+	return fetch(url, {
+		method: "DELETE",
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	})
+		.then((r) => r.json())
+		.catch((e) => {
+			console.log("Error with DELETE fetcher: " + e);
+		});
+};
+
+export {
+	apiFetcher,
+	tokenFetcher,
+	imageFetcher,
+	postFetcher,
+	putFetcher,
+	deleteFetcher,
+};
