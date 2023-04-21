b3a9edbe01226a849fabe39499d87107dbb1e3a2
williammleziva <willmlez@gmail.com>
2023-04-21 04:47:58 -0500

    form camera feature

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,8 +1,9 @@
 import { React, useRef, useState } from 'react';
-import Webcam from 'react-webcam';
+import Camera from './Camera'
 
 export default function AddForm(){
     const [stage, setStage] = useState(1);
+    const [cameraOpen, setcameraOpen] = useState(false);
     const [photo, setPhoto] = useState(null);
     const webcamRef = useRef(null);
 
@@ -11,16 +12,22 @@ export default function AddForm(){
         event.preventDefault();
         const formData = new FormData(event.target);
         formData.append('photo', photo);
+        const data = {};
+        for (let [key, value] of formData.entries()) {
+            data[key] = value;
+        }
+        console.log(JSON.stringify(data));
         // submit form data to API or backend
     };
     
-    const handleTakePhoto = () => {
-        const photo = webcamRef.current.getScreenshot();
+    const handleTakePhoto = (photo) => {
         setPhoto(photo);
+        setcameraOpen(false);
       };
     
     const handleClearPhoto = () => {
         setPhoto(null);
+        setcameraOpen(false);
     };
     
     const handleUploadPhoto = (event) => {
@@ -44,44 +51,39 @@ export default function AddForm(){
                                     <img src={photo} alt="User" />
                                     <br />
                                     <button className="button is-danger" type="button" onClick={handleClearPhoto}>Clear Photo</button>
+                                    <div className="field">
+                                        <div className="control">
+                                        <button className="button is-primary" type="button" onClick={() => setStage(2)}>Next</button>
+                                        </div>
+                                    </div>
                                 </div>
                             ) : (
                                 <div>
-                                    <div className="field">
-                                        <div className="file is-info has-name">
-                                            <label className="file-label">
-                                                <input className="file-input" type="file" name="photo" onChange={handleUploadPhoto} required />
-                                                <span className="file-cta">
-                                                    <span className="file-icon">
-                                                        <i className="fas fa-upload"></i>
+                                    {cameraOpen ? (
+                                        <Camera onCapture={handleTakePhoto}/>
+                                    ):(
+                                        <div className="field">
+                                            <div className="file is-info has-name">
+                                                <label className="file-label">
+                                                    <input className="file-input" type="file" name="photo" onChange={handleUploadPhoto} required />
+                                                    <span className="file-cta">
+                                                        <span className="file-icon">
+                                                            <i className="fas fa-upload"></i>
+                                                        </span>
+                                                        <span className="file-label">
+                                                            Upload Photo
+                                                        </span>
                                                     </span>
-                                                    <span className="file-label">
-                                                        Upload Photo
+                                                    <span className="file-name">
+                                                        {photo ? photo.name : 'No file selected'}
                                                     </span>
-                                                </span>
-                                                <span className="file-name">
-                                                    {photo ? photo.name : 'No file selected'}
-                                                </span>
-                                            </label>
+                                                </label>
+                                            </div>
+                                            <button className="button is-primary" type="button" onClick={() => setcameraOpen(true)}>Open Camera</button>
                                         </div>
-                                    </div>
-                                    <div className="field">
-                                        <div className="control">
-                                            <button className="button is-info" type="button" onClick={handleTakePhoto}>Take Photo</button>
-                                        </div>
-                                    </div>
-                                    <Webcam
-                                        audio={false}
-                                        ref={webcamRef}
-                                        screenshotFormat="image/jpeg"
-                                    />
+                                    )}
                                 </div>
                             )}
-                        </div>
-                        <div className="field">
-                            <div className="control">
-                                <button className="button is-primary" type="button" onClick={() => setStage(2)}>Next</button>
-                            </div>
                         </div>
                     </div>
                 )}
@@ -90,7 +92,7 @@ export default function AddForm(){
                         <div class="field">
                             <label class="label">Item Name</label>
                             <div class="control">
-                                <input class="input" type="text" placeholder="" required></input>
+                                <input class="input" name="itemname" type="text" placeholder="" required></input>
                             </div>
                         </div>
                         <div class="field">
@@ -124,11 +126,11 @@ export default function AddForm(){
                             <label class="label">Own</label>
                             <div class="control">
                                 <label class="radio">
-                                    <input type="radio" name="own" required></input>
+                                    <input type="radio" name="own" value="yes" required></input>
                                     Yes
                                 </label>
                                 <label>
-                                    <input type="radio" name="own" required></input>
+                                    <input type="radio" name="own" value="no" required></input>
                                     No
                                 </label>
                             </div>
